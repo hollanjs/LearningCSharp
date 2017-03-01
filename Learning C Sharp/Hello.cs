0a93858fabefb41e440b8ba2c609bdba7dd836e1
Josh Hollandsworth <jshollandsworth@gmail.com>
2017-03-01 13:19:28 -0500

    Initial Commit Hello World added, them pushed to seperate class. Just playing around with using classes.

---
+++ Learning C Sharp/Hello.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Learning_C_Sharp
+{
+    public class Hello
+    {
+        
+        public string name { get; set; }
+
+        public void sayHi ()
+        {
+            Console.WriteLine("\nHello, {0}!\a", name);
+        }
+
+        public void getName()
+        {
+            Console.Write("What is your name?\n-->  ");
+            name = Console.ReadLine();
+        }
+
+        public void finishHim()
+        {
+            Console.WriteLine("\nAlright, I think we're done here {0}.", name);
+        }
+
+        public Hello(string n = "Person of the World")
+        {
+            name = n;
+        }
+
+    }
+}
---
+++ Learning C Sharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Learning_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testHello();
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadLine();
+        }
+
+        static void testHello()
+        {
+            Hello Hi1 = new Hello();
+            Hello Hi2 = new Hello("Stephen");
+            Hello Hi3 = new Hello();
+
+            Hi1.sayHi();
+            Hi1.getName();
+            Hi1.sayHi();
+            Hi1.finishHim();
+
+            Console.WriteLine("\nNext person...");
+
+            Hi2.sayHi();
+            Hi2.finishHim();
+
+            Console.WriteLine("\nNext person...");
+
+            Hi3.sayHi();
+            Hi3.finishHim();
+
+            Console.WriteLine("\nFinally, we're done, done, done!");
+        }
+    }
+}
